d3fdfe3ea9667240763f1c8b522b490a53a82e9e
Isa Levine <isaacdlevine@gmail.com>
2019-01-17 04:39:24 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  counter = 0
+  collection = array.length
+  while counter <= collection do |items|
+    yield
+    counter += 1
+  end    
 end
